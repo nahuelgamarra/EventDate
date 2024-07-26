0522b5f2fa2a4d58b1ee646c080bc56c1300fe50
Nahuel Gamarra <gamarraorlandonahuel@gmail.com>
2024-07-26 18:13:06 -0300

    add ms-event-catalog-service

---
+++ ms-event-catalog-service/src/main/java/com/eventdate/catalog/model/Event.java
@@ -0,0 +1,4 @@
+package com.eventdate.catalog.model;
+
+public class Event {
+}
---
+++ ms-event-catalog-service/src/test/java/com/eventdate/catalog/MsEventCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventdate.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEventCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
