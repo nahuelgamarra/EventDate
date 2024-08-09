b98ac8aca099cfa35f72699ceb4f5302572b2df2
Nahuel Gamarra <gamarraorlandonahuel@gmail.com>
2024-08-09 15:43:00 -0300

    getReservationByUser added

--- ms-reservation-service/src/main/java/com/eventdate/msreservationservice/controller/ReservationController.java
+++ ms-reservation-service/src/main/java/com/eventdate/msreservationservice/controller/ReservationController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,17 +25,17 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
-    @GetMapping("/user")
-    public Flux<Reservation> getReservationService() {
-        log.info("getReservation");
-        return reservationService.getReservationsByUserId(1L)
-                .switchIfEmpty(Mono.error(new IllegalArgumentException("Reservation not found")));
+    @GetMapping("/user/{userId}")
+    public Flux<Reservation> getAllReservationsByUser(@PathVariable Long userId) {
+        log.info("getAllReservationsByUser userId={}", userId);
+        return reservationService.getReservationsByUserId(userId)
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("No reservations found for user with ID " + userId)));
     }
+
 
     @PostMapping
     public Mono<ResponseEntity<Reservation>> createReservation(@RequestBody @Valid ReservationRequest reservation) {
         log.debug("createReservation {}", reservation);
-
         return reservationService.create(reservation)
                 .map(reservationResponse -> new ResponseEntity<>(reservationResponse, HttpStatus.CREATED))
                 .defaultIfEmpty(new ResponseEntity<>(HttpStatus.BAD_REQUEST));
